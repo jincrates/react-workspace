796867dc5ca0299c67d6d77f00d0423368492556
jincrates <94jingyu@gmail.com>
2021-11-12 23:09:57 +0900

    Todo 리스트 생성

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Todo from './Todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todo />
-      <Todo />
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        // (1) item -> items 배열로
+        this.state = {
+            items: [
+                { id: "0", title: "Hello World 0", done: true },
+                { id: "1", title: "Hello World 1", done: false },
+            ],
+        };
+    }
+
+    render() {
+        // (2) 자바스크립트가 제공하는 map 함수를 이용해 배열을 반복하여 <Todo.../> 컴포넌트 생성
+        var todoItems = this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+        ));
+
+        // (3) 생성된 컴포넌트 리턴
+        return <div className = "App" >{todoItems}</div>;
+    }
 }
 
 export default App;
