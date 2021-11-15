b46388f90195f25110a0ec7f85ee97b3abe19a14
jincrates <94jingyu@gmail.com>
2021-11-15 11:01:24 +0900

    modify, checkbox event 추가

--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -12,7 +12,7 @@ import DeleteOutLined from "@material-ui/icons/DeleteForeverOutlined";
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { item: props.item };
+        this.state = { item: props.item, readOnly: true };
         this.delete = props.delete;
     }
 
@@ -21,20 +21,52 @@ class Todo extends React.Component {
         this.delete(this.state.item);
     }
 
+    offReadOnlyMode = () => {
+        console.log("Event!", this.state.readOnly);
+        this.setState({ readOnly: false }, () => {
+            console.log("ReadOnly? ", this.state.readOnly);
+        })
+    }
+
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.setState({readOnly: true});
+        }
+    }
+
+    //사용자가 키보드로 입력할 때마다 item을 새 값으로 변경함
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+    }
+
+    checkboxEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = !thisItem.done;
+        this.setState({ item: thisItem });
+    }
+
     render() {
         const item = this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done} />
+                <Checkbox checked={item.done} onChange={this.checkboxEventHandler} />
                 <ListItemText>
                     <InputBase 
-                        inputProps={{ "aria-label": "naked" }}
+                        inputProps={{
+                            "aria-label": "naked", 
+                            readOnly: this.state.readOnly,
+                        }}
                         type="text"
                         id={item.id}  // 각 리스트를 구분하려고 id를 연결
                         name={item.id} // 각 리스트를 구분하려고 id를 연결
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
+                        onClick={this.offReadOnlyMode}
+                        onChange={this.editEventHandler}
+                        onKeyPress={this.enterKeyEventHandler}
                     />
                 </ListItemText>
 
