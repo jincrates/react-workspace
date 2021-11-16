348f41b5ec58bf03c677d50e5753df914dea76b8
jincrates <94jingyu@gmail.com>
2021-11-17 08:36:49 +0900

    add cogito app

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Todo from './Todo';
-import AddTodo from './AddTodo';
-import { Paper, List, Container } from "@material-ui/core" 
+import React from "react";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
+import "./App.css";
 import { call } from "./service/ApiService";
-import './App.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [ ],  // items를 빈 리스트로 초기화
+            items: [],
         };
     }
 
@@ -19,34 +19,33 @@ class App extends React.Component {
         );
     }
 
-    // (1) 함수 추가
     add = (item) => {
-        call("/todo", "POST", item).then((response) => 
+        call("/todo", "POST", item).then((response) =>
             this.setState({ items: response.data })
         );
-    }
+    };
 
     delete = (item) => {
         call("/todo", "DELETE", item).then((response) =>
             this.setState({ items: response.data })
         );
-    }
+    };
 
     update = (item) => {
         call("/todo", "PUT", item).then((response) =>
             this.setState({ items: response.data })
         );
-    }
+    };
 
     render() {
         var todoItems = this.state.items.length > 0 && (
             <Paper style={{ margin: 16 }}>
                 <List>
                     {this.state.items.map((item, idx) => (
-                        <Todo 
-                            item={item} 
-                            key={item.id} 
-                            delete={this.delete} 
+                        <Todo
+                            item={item}
+                            key={item.id}
+                            delete={this.delete}
                             update={this.update}
                         />
                     ))}
@@ -54,9 +53,9 @@ class App extends React.Component {
             </Paper>
         );
 
-        // (2) 함수 연결
+        // 3. props로 넘겨주기
         return (
-            <div className="App" >
+            <div className="App">
                 <Container maxWidth="md">
                     <AddTodo add={this.add} />
                     <div className="TodoList">{todoItems}</div>
--- todo-react-app/src/service/ApiService.js
+++ todo-react-app/src/service/ApiService.js
@@ -14,13 +14,31 @@ export function call(api, method, request) {
         options.body = JSON.stringify(request);
     }
 
-    return fetch(options.url, options).then((response) => 
-        response.json().then((json) => {
-            if (!response.ok) {
-                // reponse.ok가 true이면 정상적인 응답을 받은 것이고 아니면 에러 응답을 받은 것임
-                return Promise.reject(json);
+    return fetch(options.url, options)
+        .then((response) => 
+            response.json().then((json) => {
+                if (!response.ok) {
+                    // reponse.ok가 true이면 정상적인 응답을 받은 것이고 아니면 에러 응답을 받은 것임
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        )
+        .catch((error) => {
+            // 추가된 부분
+            console.log(error.status);
+            if (error.status === 403) {
+                window.location.href = "/login"; //redirect
             }
-            return json;
-        })
-    );
+
+            return Promise.reject(error);
+        });
+}
+
+export function signin(userDTO) {
+    return call("/auth/sign", "POST", userDTO)
+        .then((response) => {
+            console.log("response: ", response);
+            alert("로그인 토큰: " + response.token);
+        });
 }
