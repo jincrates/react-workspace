a54a6f35b9a7a20f42ae72a1c45dc91b6723fcee
jincrates <94jingyu@gmail.com>
2022-04-16 00:04:22 +0900

    material ui를 이용한 Todo 컴포넌트 디자인

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Todo from './Todo';
+import { Paper, List } from "@material-ui/core";
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todo />
-      <Todo />
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //1. item -> items 배열로
+            items: [
+                { id: "0", title: "Hello World 1", done: true },
+                { id: "1", title: "Hello World 2", done: false },
+            ],
+        };
+    }
+
+    render() {
+        //2. 자바스크립트가 제공하는 map 함수를 이용해 배열을 반복하여 Todo 컴포넌트 생성
+        var todoItems = this.state.items.length > 0 && (
+            <Paper style={{ margin: 16 }}>
+                <List>
+                    {this.state.items.map((item, idx) => (
+                        <Todo item={item} key={item.id} />
+                    ))}
+                </List>
+            </Paper>
+        );
+        
+        //3. 생성된 컴포넌트 리턴
+        return <div className="App">{todoItems}</div>;
+    }
 }
 
 export default App;
